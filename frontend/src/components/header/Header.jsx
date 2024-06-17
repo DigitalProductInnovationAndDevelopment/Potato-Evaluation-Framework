8b257a7953335b8b095fd4c1ca7baf184748f194
Umut Oezdemir <umut.oezdemir@temedica.com>
2024-06-17 13:56:31 +0200

    Introduce logout button

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,15 +3,16 @@ import './header.css';
 import Logo from '../logo/Logo';
 import Nav from '../navigator/Nav'
 
-function Header() {
+const Header = ({ onLogout }) => {
   return (
-    <header id='header' className='header fixed-top d-flex align-items-center'>
-      {/* {logo} */}
-      <Logo />
-      {/* {search bar} */}
-      {/* {nav} */}
-      <Nav />
-    </header>
+      <header id='header' className='header fixed-top d-flex align-items-center'>
+          {/* {logo} */}
+          <Logo/>
+          {/* {search bar} */}
+          {/* {nav} */}
+          <Nav/>
+          <button className="logout-button" onClick={onLogout}>Logout</button>
+      </header>
   )
 }
 
