31e194d382bc54c71e26a71f2eceb70af0bb901e
Umut Oezdemir <ozdemir.umut98@gmail.com>
2024-08-10 14:53:00 +0200

    Refactor test files

--- frontend/src/__tests__/MainDashboard.test.js
+++ frontend/src/__tests__/MainDashboard.test.js
@@ -7,23 +7,6 @@ import MainDashboard from "../components/mainDashboard/MainDashboard";
 jest.mock('../components/parameterSelection/ParameterSelection', () => () => <div>Parameter Selection Component</div>);
 jest.mock('../components/statistics/Statistics', () => () => <div>Statistics Component</div>);
 
-it('renders the mainDashboard component', () => {
-    render(<MainDashboard />);
-
-    expect(screen.getByText('Parameter Selection Component')).toBeInTheDocument();
-
-    expect(screen.getByText('Statistics Component')).toBeInTheDocument();
-});
-
-it('applies the correct classes to the dashboard', () => {
-    const { container } = render(<MainDashboard />);
-
-    // Check if the main dashboard div has the correct ID and class
-    const dashboardElement = container.querySelector('#dashboard');
-    expect(dashboardElement).toBeInTheDocument();
-    expect(dashboardElement).toHaveClass('dashboard');
-});
-
 it('renders two columns', () => {
     render(<MainDashboard />);
     expect(true).toBeTruthy();
